5fd594841f0f6bf07d743a76ff6a72cc596e44df
amyhenning <henning.amy@gmail.com>
2019-03-12 22:18:34 +0000

    Updated orbital period solution to remove unnecessary console.log statement

--- orbitalPeriod.js
+++ orbitalPeriod.js
@@ -7,7 +7,6 @@
 function orbitalPeriod(arr) {
   var GM = 398600.4418;
   var earthRadius = 6367.4447;
-  console.log(period);
   var newArr = [];
   for (var i = 0; i < arr.length; i++) {
     var period = Math.round(2 * Math.PI * (Math.sqrt(Math.pow((earthRadius + arr[i].avgAlt), 3)/GM)));
